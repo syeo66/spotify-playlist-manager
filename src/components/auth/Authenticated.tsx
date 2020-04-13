8ecd1be4869ab0d88880def711fb57eeccf9097e
Red Ochsenbein <red@redochsenbein.ch>
2020-04-13 18:02:21 +0200

    Move Authenticated.js to Typescript

---
+++ src/components/auth/Authenticated.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import Signin from '../Signin';
+
+interface AuthenticatedProps {
+  auth: string | boolean;
+}
+const Authenticated: React.FC<AuthenticatedProps> = ({ auth, children }) => {
+  return !auth ? <Signin /> : <>{children}</>;
+};
+
+interface MapStateToPropsInput {
+  auth: string | boolean;
+}
+const mapStateToProps = ({ auth }: MapStateToPropsInput) => {
+  return { auth };
+};
+
+export default connect(mapStateToProps, {})(Authenticated);
