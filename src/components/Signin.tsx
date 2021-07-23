f7719223b334ac5c9e455a1ca7802ad3b0825f17
Red Ochsenbein <red@redochsenbein.ch>
2021-07-23 15:27:46 +0200

    improved signin screen

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -15,7 +15,15 @@ const SigninBox = styled.section`
   display: flex;
   justify-content: center;
   box-shadow: 0 0.2rem 0.2rem ${colors.shadow}, 0 0.6rem 0.6rem ${colors.shadowPrimary};
-  margin: 25vh auto 0;
+  margin: 1rem auto;
+`
+
+const SigninScreen = styled.div`
+  position: absolute;
+  inset: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 interface SigninProps {
@@ -24,13 +32,29 @@ interface SigninProps {
 
 const Signin: React.FC<SigninProps> = () => {
   return (
-    <SigninBox>
-      <Button onClick={signInWithSpotify}>
-        <FontAwesomeIcon icon={faSpotify} />
-        &nbsp;Sign In With Spotify
-      </Button>
-    </SigninBox>
+    <SigninScreen>
+      <SigninBox>
+        <Button onClick={signInWithSpotify}>
+          <FontAwesomeIcon icon={faSpotify} />
+          &nbsp;Sign In With Spotify
+        </Button>
+      </SigninBox>
+      <SigninInfo>
+        This website is communicating with the Spotify API only. It never sends or stores any data to its own server.
+      </SigninInfo>
+    </SigninScreen>
   )
 }
+
+const SigninInfo = styled.div`
+  position: absolute;
+  bottom: 0;
+  padding: 1rem;
+  text-align: center;
+  font-size: 95%;
+  color: rgba(0, 0, 0, 0.5);
+  left: 0;
+  right: 0;
+`
 
 export default memo(Signin)
